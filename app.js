d0a5aab3f0c0a2d2c1bc7f12f34c80e8f9adf1e6
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-03-29 19:35:02 +0300

    feat: do user content

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+// const routes = require('./routes');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(routes);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -15,5 +17,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
