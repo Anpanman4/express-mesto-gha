dc39cbf59c6ea8883d9b488cb8c2f324c0d4223a
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-03-29 12:52:53 +0300

    feat: do prepare lints and prepare app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT);
