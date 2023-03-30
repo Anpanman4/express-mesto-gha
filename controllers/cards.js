cf78a987da418fb72948c19df64b13b22c925a5c
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-03-30 20:44:05 +0300

    feat: do const errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,23 +1,27 @@
 const Card = require('../models/card');
 
+const SUCCEES_CODE = 200;
+const SUCCEES_CREATE = 201;
+const ERROR_SERVER = 500;
+
 const getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.send(cards))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((cards) => res.status(SUCCEES_CODE).send(cards))
+    .catch((err) => res.status(ERROR_SERVER).send({ message: err.message }));
 };
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(200).send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => res.status(SUCCEES_CREATE).send(card))
+    .catch((err) => res.status(ERROR_SERVER).send({ message: err.message }));
 };
 
 const deleteCard = (req, res) => {
   const { id } = req.params;
   Card.findByIdAndDelete(id)
-    .then(() => res.status(200).send('Карточка успешно удалена'))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then(() => res.status(SUCCEES_CODE).send('Карточка успешно удалена'))
+    .catch((err) => res.status(ERROR_SERVER).send({ message: err.message }));
 };
 
 const likeCard = (req, res) => {
@@ -26,8 +30,8 @@ const likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .then(() => res.status(200).send('Лайк успешно поставлен'))
-    .catch((err) => res.status(400).send({ message: err.message }));
+    .then(() => res.status(SUCCEES_CODE).send('Лайк успешно поставлен'))
+    .catch((err) => res.status(ERROR_SERVER).send({ message: err.message }));
 };
 
 const dislikeCard = (req, res) => {
@@ -36,8 +40,8 @@ const dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .then(() => res.status(200).send('Лайк успешно убран'))
-    .catch((err) => res.status(400).send({ message: err.message }));
+    .then(() => res.status(SUCCEES_CODE).send('Лайк успешно убран'))
+    .catch((err) => res.status(ERROR_SERVER).send({ message: err.message }));
 };
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,29 @@
 const User = require('../models/user');
 
+const SUCCEES_CODE = 200;
+const SUCCEES_CREATE = 201;
+const ERROR_SERVER = 500;
+
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => {
-      res.send({ data: users });
-    })
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((users) => res.status(SUCCEES_CODE).send({ data: users }))
+    .catch((err) => res.status(ERROR_SERVER).send({ message: err.message }));
 };
 
 const getUserById = (req, res) => {
   const { id } = req.params;
 
   User.findById(id)
-    .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((users) => res.status(SUCCEES_CODE).send({ data: users }))
+    .catch((err) => res.status(ERROR_SERVER).send({ message: err.message }));
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(SUCCEES_CREATE).send({ data: user }))
+    .catch((err) => res.status(ERROR_SERVER).send({ message: err.message }));
 };
 
 const updateUserInfo = (req, res) => {
@@ -31,8 +33,8 @@ const updateUserInfo = (req, res) => {
     new: true,
     runValidators: true,
   })
-    .then((user) => res.status(200).send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(SUCCEES_CODE).send(user))
+    .catch((err) => res.status(ERROR_SERVER).send({ message: err.message }));
 };
 
 const updateUserAvatar = (req, res) => {
@@ -42,8 +44,8 @@ const updateUserAvatar = (req, res) => {
     new: true,
     runValidators: true,
   })
-    .then((user) => res.status(200).send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => res.status(SUCCEES_CODE).send(user))
+    .catch((err) => res.status(ERROR_SERVER).send({ message: err.message }));
 };
 
 module.exports = {
