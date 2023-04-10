fee30227e1df130f72a1be673fdfdf885cf47ff0
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-04-10 23:09:53 +0300

    fix: validator create user

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const auth = require('../middlewares/auth');
 const { ERROR_ID } = require('../utils/utils');
@@ -8,7 +9,15 @@ const cardRoutes = require('./cards');
 const { login, createUser } = require('../controllers/users');
 
 router.post('/signin', login);
-router.post('/signup', createUser);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().required().pattern(/https?:\/\/(www\.)?[A-Za-z0-9\.\-\/]{1,}/g),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 
 router.use(auth);
 
