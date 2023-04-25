24b0125b0abe59896700079826d86acb094d9725
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-04-25 20:49:27 +0300

    feat: do add loggers

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ const routes = require('./routes');
 const globalError = require('./middlewares/globalError');
 
 const { PORT } = require('./utils/utils');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(requestLogger);
 
 app.use(routes);
 
+app.use(errorLogger);
 app.use(errors());
 app.use(globalError);
 
